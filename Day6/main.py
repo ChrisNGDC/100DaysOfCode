ae4a98116212938a9e2e19e6ef2444662a77b56d
ChrisGDC <chrisgabor95@gmail.com>
2023-09-14 13:28:23 -0300

    Day 6

---
+++ Day6/main.py
@@ -0,0 +1,20 @@
+# def turn_r():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def turn_around():
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     elif right_is_clear():
+#         turn_r()
+#     else:
+#         turn_left()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
