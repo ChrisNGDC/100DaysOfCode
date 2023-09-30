b8563bef3679074352a117e40c621315f698e886
Christian Gabor Delli Carri <chrisgabor95@gmail.com>
2023-09-30 15:46:59 -0300

    Day 30 update

--- Day26/main.py
+++ Day26/main.py
@@ -3,8 +3,13 @@ import pandas as p
 phonetic_data = p.read_csv('./nato_phonetic_alphabet.csv')
 phonetic_dict = {row.letter: row.code for (index, row) in phonetic_data.iterrows()}
 
-word = input('Enter a word → ').upper()
-
-nato_phonetic_word = [phonetic_dict[letter.upper()] for letter in word if letter in phonetic_dict]
+while True:
+    word = input('Enter a word → ').upper()
+    try:
+        nato_phonetic_word = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Only letters are valid.")
+    else:
+        break
 
 print(nato_phonetic_word)
