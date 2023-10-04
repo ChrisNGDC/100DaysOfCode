5a4bb02b6624601a88ff522e7530ae46ae3370ac
Christian Gabor <chrisgabor95@gmail.com>
2023-10-04 16:10:03 -0300

    Day 32

---
+++ Day32/main.py
@@ -0,0 +1,35 @@
+import smtplib
+import datetime as dt
+import random as r
+import pandas as p
+
+
+mail = 'chris.code.testing@gmail.com'
+password = input('Enter the password: ')
+
+
+def send_mail(name, email):
+    with open(f'./letter_templates/letter_{r.randint(1,3)}.txt') as letter_file:
+        letter = letter_file.read().replace('[NAME]', name)
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as conn:
+        conn.starttls()
+        conn.login(user=mail, password=password)
+        conn.sendmail(from_addr=mail, to_addrs=email, msg='Subject: Happy Birthday!!!\n\n' + letter)
+
+
+now = dt.date.today()
+today = now.month, now.day
+birthdays = p.read_csv('./birthdays.csv')
+birthdays_dict = {}
+for (index, row) in birthdays.iterrows():
+    birthdate = row.month, row.day
+    if not birthdays_dict.get(birthdate):
+        birthdays_dict[birthdate] = [row]
+    else:
+        birthdays_dict[birthdate].append(row)
+
+for birthday in birthdays_dict:
+    if birthday == today:
+        for people in birthdays_dict[birthday]:
+            send_mail(people['name'], people['email'])
